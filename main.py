10914248e567b2bf98fac45d2700d81921215cdf
justintran12 <jttranjustin@gmail.com>
2023-01-29 12:01:41 -0800

    fixed printReport function to sort by largest value, moved start-up code to main

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-path_to_file = "/home/justintran/workspace/github.com/justintran12/bookbot/books/frankenstein.txt"
-with open(path_to_file) as f:
-    file_contents = f.read()
-
 # return number of words in document
 def countWords(file_contents):
     words = file_contents.split()
@@ -20,20 +16,34 @@ def countLetters(file_contents):
     return letter_freqs
 
 # return report of number of words and number of letters found in document sorted by highest frequency
-def printReport(file_contents):
-    numWords = countWords(file_contents)
+def printReport(file_contents, book_name):
+    num_words = countWords(file_contents)
     letters = countLetters(file_contents)
-    lettersList = letters.keys()
+    # convert dictionary to list and sort by value
+    sorted_letters = sorted(countLetters(file_contents).items(), key = lambda x:x[1], reverse = True)
 
-    print("%d words were found in the document" % numWords)
+    print("--- Report of %s ---" % book_name)
+    print("%d words were found in the document" % num_words)
     print("\n")
-    for letter in lettersList:
-        if (letter.isalpha() == True):
-            print("The letter \'%s\' was found %d times" % (letter, letters.get(letter)))
+    # sorted_letters is a list of tuples, each tuple is a key/value pair
+    for letter_pair in sorted_letters:
+        if (letter_pair[0].isalpha() == True):
+            print("The letter \'%s\' was found %d times" % (letter_pair[0], letter_pair[1]))
 
+    print("--- End of Report ---")
 
-#print(countWords(file_contents))
-#print(countLetters(file_contents))
-printReport(file_contents)
-print(countLetters(file_contents).items())
+def main():
+    path_to_file = "/home/justintran/workspace/github.com/justintran12/bookbot/books/frankenstein.txt"
+    path_split = path_to_file.split("/")
+    book_name = path_split[len(path_split) - 1]
+    with open(path_to_file) as f:
+        file_contents = f.read()
+
+    #print(countWords(file_contents))
+    #print(countLetters(file_contents))
+    printReport(file_contents, book_name)
+    
+# only executes if running as a script on command line not importing
+if __name__ == "__main__":
+    main()
 
